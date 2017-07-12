ea505a9e04ba1e0cbd4c34314b7eabff46b3ff83
root <root@localhost.localdomain>
2017-07-12 15:13:20 +0800

    add

--- feature/originalfeatures.py
+++ feature/originalfeatures.py
@@ -2,30 +2,51 @@
 from util.redisinfos import excute
 
 class OriginalFeatures():
+    def __init__(self):
+        self.oids = list()
+        self.labelres = dict()
 
     def load(self):
-        oids=[]
+        oids = []
         oidfile = '/data1/yingjie10/selectgroupout/oidfile.txt'
         files = open(oidfile, 'r')
         for line in files:
-            oid=line.strip()
+            oid = line.strip()
             oids.append(oid)
-        return oids
+        self.oids = oids
 
-    def extractfea(self,oids):
-        redisinfos=excute(oids)
-        feafile= '/data1/yingjie10/selectgroupout/feafile.txt'
-        files = open(feafile, 'w')
+        labelres = dict()
+        labelfile = '/data1/yingjie10/selectgroupout/labelfile.txt'
+        file = open(labelfile, 'r')
+        for line in file:
+            temp = line.strip().split(',')
+            gid = temp[0]
+            oid = temp[1]
+            label = temp[2]
+            labelres[oid]=dict()
+            labelres[oid]['gid']=gid
+            labelres[oid]['label'] = label
+        self.labelres = labelres
+
+    def extractfea(self):
+        redisinfos=excute(self.oids)
+        train= '/data1/yingjie10/selectgroupout/train.txt'
+        files = open(train, 'w')
+        i=int(0)
         for oid in redisinfos:
-            files.write(oid+';')
-            files.write(str(redisinfos[oid])+'\n')
+            i+=1
+            gid=self.labelres[oid]['gid']
+            label=self.labelres[oid]['label']
+            if i==1:
+                files.write('label,oid,gid,'+str(redisinfos[oid].keys()).replace('[','').replace(']','').replace("'",'')+'\n')
+            files.write(label+','+oid+','+gid+','+str(redisinfos[oid].values()).replace('[', '').replace(']', '') + '\n')
         files.close()
     def excute(self):
-        oids = self.load()
-        self.extractfea(oids)
+        self.load()
+        self.extractfea()
 if __name__=='__main__':
     fea=OriginalFeatures()
-    oids=fea.load()
-    fea.extractfea(oids)
+    fea.extractfea()
 
     # fea.extractfea(fea.loader.alloids)
+
