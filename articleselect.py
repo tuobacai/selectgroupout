b8381deb7fd66eee511605bd3813a426a3e29194
yingjie10 <yingjie10@staff.weibo.com>
2017-07-19 17:01:40 +0800

    add model

--- articleselect.py
+++ articleselect.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 from featurehandler import FeatureHandler
 from modelhandler import ModelHandler
-class Main(object):
+class ArticleSelect(object):
     def __init__(self):
         self.feahandelr=FeatureHandler()
         self.modelhandler=ModelHandler()
@@ -9,5 +9,8 @@ class Main(object):
     def excute(self):
         self.oid2info=self.feahandelr.excute()
         self.weights=self.modelhandler.excute(self.oid2info)
+if __name__=="__main__":
+    select=ArticleSelect()
+    select.excute()
 
 
